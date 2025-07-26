e8c8c2071f82b0494311700e350140cf6b839d33
xWarrainx <Arain1@rambler.ru>
2025-07-26 13:28:01 +0000

    Update webpack.common.js

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -9,12 +9,12 @@ const production = process.env.NODE_ENV === 'production';
 module.exports = {
 	entry: path.resolve(__dirname, '..', './src/index.tsx'), //точка входа в наше приложение содержит абсолютный путь к index.ts
 	output: {
-		path: path.resolve(__dirname, '..', './dist'), //путь куда будет собираться наш проект
-		filename: production
-			? 'static/scripts/[name].[contenthash].js'
-			: 'static/scripts/[name].js', // имя нашего бандла
-		publicPath: '/',
-		chunkFilename: 'static/scripts/[name].[contenthash].bundle.js'
+	    path: path.resolve(__dirname, '..', './dist'), // путь, по которому будет собираться наш проект
+	    filename: production
+	        ? 'static/scripts/[name].[contenthash].js'
+	        : 'static/scripts/[name].js', // имя нашего бандла
+	    publicPath: process.env.PUBLIC_PATH ? process.env.PUBLIC_PATH : '/', // указываем путь, который будет добавляться перед подключением файлов
+	    chunkFilename: 'static/scripts/[name].[contenthash].bundle.js'
 	},
 	//Нужно помочь вебпаку научится работать с jsx и tsx файлами для этого используют ts loader
 	module: {
@@ -89,7 +89,8 @@ module.exports = {
 				: 'static/styles/[name].css',
 		}),
 		new webpack.EnvironmentPlugin({
-			NODE_ENV: 'development', // значение по умолчанию 'development' если переменная process.env.NODE_ENV не передана
-		}),
+            PUBLIC_PATH: null, // значение по умолчанию null, если переменная process.env.PUBLIC_PATH не передана
+            NODE_ENV: 'development', // значение по умолчанию 'development', если переменная process.env.NODE_ENV не передана
+        }),
 	],
 };
